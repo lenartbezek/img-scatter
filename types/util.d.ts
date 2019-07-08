c9d358f667ec05bd06fb8e8e7448f19276f7b6d7
Lenart Bezek <lench4991@gmail.com>
2019-07-08 22:34:04 +0200

    Delete fiels that should be ignored.

--- types/util.d.ts
+++
@@ -1,3 +0,0 @@
-import { Vector3 } from "three";
-export declare function rgbToHsl(r: number, g: number, b: number): number[];
-export declare function setBufferFromVector(buffer: Float32Array, index: number, vector: Vector3): void;
